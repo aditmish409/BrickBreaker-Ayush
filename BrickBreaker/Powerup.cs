682d9c08069508470901b9e252c1d42c87111c01
aditmish409 <aditmish409@SDS-L-262Q00>
2025-05-06 14:10:22 -0400

    added powerup class

---
+++ BrickBreaker/Powerup.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrickBreaker
+{
+    internal class Powerup
+    {
+        public int x, y;
+        public int size = 5;
+        public int ySpeed;
+        //gameBall = new Ball(GameScreen.gameScreenWidth / 2, GameScreen.Height - 40, 10, -8);
+
+        List<Ball> balls = new List<Ball>();
+        //List<Bricks> bricksList = new List<Bricks>();
+
+        public Powerup(int _x, int _y, int _xSpeed, int _ySpeed)
+        {
+            x = _x;
+            y = _y;
+            ySpeed = _ySpeed;
+        }
+
+        public void Move()
+        {
+            y += ySpeed;
+        }
+
+        private void firePU()
+        {
+            //if (gameBall.IntersectsWit
+        }
+    }
+}
